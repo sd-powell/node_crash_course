a37e63887eb097cb789ce29c7b8a71aca71cb7bd
sd-powell <kf210rhd@students.codeinstitute.net>
2025-11-10 19:41:03 +0000

    complete os module

---
+++ osDemo.js
@@ -0,0 +1,13 @@
+import os from 'os';
+
+// userInfo()
+console.log('User Info:', os.userInfo());
+
+// totalmem()
+console.log('Total Memory:', os.totalmem(), 'bytes');
+
+// freemem()
+console.log('Free Memory:', os.freemem(), 'bytes');
+
+//cpus()
+console.log('CPU Info:', os.cpus());
